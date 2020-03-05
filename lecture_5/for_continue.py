6a8eaed4d97b41484327429a6c3d2f2924be3703
intelligentracinginc <allen@intelligentracing.com>
2020-03-04 22:35:05 -0800

    Create for_continue.py

---
+++ lecture_5/for_continue.py
@@ -0,0 +1,18 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 5 Example code: while_not_quit.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+animal_list = ['dog', 'cat','fish','pony','parrot','leopard','frog','mouse','snake']
+
+index = 0
+adopted_animals=[]
+for i in animal_list:
+    answer = input('Would you like to adopt a '+ i + '? [Y/N]')
+    if answer.lower()!='y':
+        continue
+    
+    adopted_animals.append(i)
+
+print('We will have your ' + str(adopted_animals) + ' ready for pick up!')
