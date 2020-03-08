330a56e9291161c7e6322420fff60025e1858793
intelligentracinginc <allen@intelligentracing.com>
2020-03-07 16:16:01 -0800

    syntax error and runtime error sample codes

---
+++ lecture_6/runtime_error.py
@@ -0,0 +1,20 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 6 Example code: runtime_error.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+# Most popular runtime error examples
+
+EXAMPLE_TO_RUN = 0
+
+animal_list = ['dog', 'cat', 'pony', 'fisn', 'leopard', 'rabbit','mouse']
+
+if EXAMPLE_TO_RUN == 0:
+    # ********  Exp 1: Using Reserved Words  ********
+    int = 123
+    float = int(10.57)
+elif EXAMPLE_TO_RUN == 1:
+    # ********  Exp 1: Calling methods that don't exisit  ********
+
+
---
+++ lecture_6/syntax_error.py
@@ -0,0 +1,42 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 6 Example code: syntax_error.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+# Most popular syntax error examples
+
+# ********  Case 1: Wrong text formatting  ********
+while i in range(10)        # Error
+    print(i)
+else                        # Error
+      pass
+
+# ********  Case 2: Misuse of brackets  ********
+ List = ('abc','def')        
+ print(List(1))              # Error
+
+# ********  Case 3: Wrong indentation  ********
+  x = 0
+  y = 1
+  z = 3
+
+# ********  Case 4: Format the import modules  ********
+print(sqrt(9))
+
+# ********  Case 5: Call methods in the wrong type of objects  ********
+String = "Hello World!"
+String.reverse()
+
+# ********  Case 6: Use the wrong operator on variable types
+result_string = 'abcde' + 5
+
+# ********  Case 7: Misspell a variable
+animals = ['dog', 'cat', 'pony', 'fisn', 'leopard', 'rabbit','mouse']
+print(Animal[0])
+
+# ********  Case 8: Wrong use of quotation marks
+wrong_string = 'Mike's story'
+print(wrong_string)
+
+
