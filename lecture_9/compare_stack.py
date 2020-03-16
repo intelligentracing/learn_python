43a6604c5131f08ee7134276ca879508be19c242
intelligentracinginc <allen@intelligentracing.com>
2020-03-15 18:33:24 -0700

    Create compare_stack.py

---
+++ lecture_9/compare_stack.py
@@ -0,0 +1,37 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 9 Example code: compare_stack.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+from collections import deque
+from random import sample
+from time import time
+
+# Generate a sufficiently long list for comparison
+sample_count = 1000000
+random_input = sample(range(0, sample_count),sample_count)
+
+# Test speed of enqueue and dequeue operations in list type
+test_list=[]
+begin_time = time()
+for i in random_input:
+    test_list.append(i)
+
+for i in range(sample_count):
+    test_list.pop(-1)
+
+elapsed_time = time() - begin_time
+print('List FIFO time: {}'.format(elapsed_time))
+
+# Test speed of enqueue and dequeue operations in dequeue type
+test_queue=deque()
+begin_time = time()
+for i in random_input:
+    test_queue.append(i)
+
+for i in range(sample_count):
+    test_queue.pop()
+
+elapsed_time = time() - begin_time
+print('Queue FIFO time: {}'.format(elapsed_time))
