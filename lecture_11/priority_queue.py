bd9b7d91e8d78faaba2cf2947cea08d7ecb1fdc8
intelligentracinginc <allen@intelligentracing.com>
2020-03-20 01:27:20 -0700

    Create priority_queue.py

---
+++ lecture_11/priority_queue.py
@@ -0,0 +1,32 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 11 Example code: priority_queue.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+directed_graph=[(12, [0, 1]), (5, [0,3]), (8, [0, 2]), (15, [1,5]), (16, [2, 4]), (2, [4,5]), (25, [3,5])]
+
+source_node = 0
+sink_node = 5
+
+DP_path = []
+
+is_complete = False
+search_stack = [(0, [sink_node])]
+while is_complete == False:
+    current_path = search_stack.pop()
+    current_node = current_path[1][0]
+
+    if current_node == source_node:
+        # The smallest cost path has arrived at the source
+        break
+
+    for edge in directed_graph:
+        if current_node == edge[1][1]:
+            updated_path = [edge[1][0]] + current_path[1]
+            new_path = (current_path[0] + edge[0], updated_path)
+            search_stack.append(new_path)
+    
+    search_stack.sort(reverse = True)
+
+print(current_path)
