abef8df81ef7f15e3ae09c0efb696b9af3b92601
intelligentracinginc <allen@intelligentracing.com>
2020-03-22 00:01:23 -0700

    Create dictionary_example.py

---
+++ lecture_11/dictionary_example.py
@@ -0,0 +1,32 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 11 Example code: dictionary_example.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+English_to_Chinese = dict()
+
+# building a short translation dictionary
+English_to_Chinese['one'] = '一'
+English_to_Chinese['two'] = '二'
+English_to_Chinese['three'] = '三'
+
+print(English_to_Chinese['two'])
+
+print('three' in English_to_Chinese)
+
+# Build a character histogram
+
+histogram = dict()
+text = 'We can know only that we know nothing. And that is the highest degree of human wisdom.' # From War and Peace
+
+for c in text:
+    if c.isalpha():
+        c = c.lower()
+        if c in histogram:
+            histogram[c] += 1
+        else:
+            histogram[c] = 1
+
+for key in histogram:
+    print(key, histogram[key])
