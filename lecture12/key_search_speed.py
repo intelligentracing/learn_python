369743b0638990e4672ef742ebd8ae44eb77e025
intelligentracinginc <allen@intelligentracing.com>
2020-03-23 15:55:04 -0700

    Create key_search_speed.py

---
+++ lecture12/key_search_speed.py
@@ -0,0 +1,6 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 12 Example code: key_search_speed.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
