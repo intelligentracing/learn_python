b75e274aab15ec2be360b5980b990a436d44eeba
intelligentracinginc <allen@intelligentracing.com>
2020-03-27 21:24:32 -0700

    Update shape_class.py

--- lecture_13/shape_class.py
+++ lecture_13/shape_class.py
@@ -5,16 +5,20 @@
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
 
 class Shape:
+    ''' An example class that defines an empty abstract shape '''
     initiated = False
 
     def get_area(self):
+        ''' virtual method to calculate area of a shape'''
         pass
 
     @classmethod
     def is_init(cls):
+        ''' return True if a shape has been assigned to the class'''
         return cls.initiated
 
 class Square(Shape):
+    ''' A subclass of Shape, specifically for calculating square area'''
 
     def __init__(self, width):
         self.width = width
@@ -22,5 +26,6 @@ class Square(Shape):
         self.__class__.initiated = True      
     
     def get_area(self):
+        ''' Area of a square is its width times width'''
         self.area = self.width*self.width
         return self.area
