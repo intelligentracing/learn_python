f0f67ab380fc434a023e1f2c430e56be8b47bf00
intelligentracinginc <allen@intelligentracing.com>
2020-03-31 16:58:23 -0700

    Update dot_product_speed.py

--- lecture_15/dot_product_speed.py
+++ lecture_15/dot_product_speed.py
@@ -10,14 +10,14 @@ from time import time
 test_dimension = 1000000
 
 # Generate two long-dimension lists of all elements 1 and 2
-L1 = [1]*test_dimension
-L2 = [2]*test_dimension
+L1 = [1.0]*test_dimension
+L2 = [2.0]*test_dimension
 
 # Benchmark the time for one dot product
 begin_time = time()
 result = sum(i[0]*i[1] for i in zip(L1, L2))
 elapsed_time = time() - begin_time
-print('List takes {0}s to compute'.format(elapsed_time))
+print('List takes {0}s to compute {1}'.format(elapsed_time, result))
 
 # Generate two long-dimension ndarrays of all elements 1 and 2
 A1 = np.ones(test_dimension)
@@ -27,4 +27,4 @@ A2 = 2*np.ones(test_dimension)
 begin_time = time()
 result = A1.dot(A2)
 elapsed_time = time() - begin_time
-print('List takes {0}s to compute'.format(elapsed_time))
+print('List takes {0}s to compute {1}'.format(elapsed_time, result))
