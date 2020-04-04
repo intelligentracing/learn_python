aa51f01e04ffcd0b023ec0d2625bd03b7347de86
intelligentracinginc <allen@intelligentracing.com>
2020-04-04 15:20:25 -0700

    Update matplotlib_sigmoid.py

--- lecture_16/matplotlib_sigmoid.py
+++ lecture_16/matplotlib_sigmoid.py
@@ -19,15 +19,24 @@ x = np.arange(-10., 10., 0.2)
 sig = sigmoid(x)
 
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+subplot1 = fig.add_subplot(1, 2, 1)
 
 # Move left y-axis and bottim x-axis to centre, passing through (0,0)
-ax.spines['left'].set_position('center')
-ax.spines['bottom'].set_position('center')
+subplot1.spines['left'].set_position('center')
+subplot1.spines['bottom'].set_position('center')
 
 # Eliminate upper and right axes
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-
+subplot1.spines['right'].set_color('none')
+subplot1.spines['top'].set_color('none')
 plt.plot(x,sig)
+
+subplot2 = fig.add_subplot(1,2,2)
+# Move left y-axis and bottim x-axis to centre, passing through (0,0)
+subplot2.spines['left'].set_position('center')
+
+# Eliminate upper and right axes
+subplot2.spines['right'].set_color('none')
+subplot2.spines['top'].set_color('none')
+plt.plot(x,sig)
+
 plt.show()
