28616c50180b2fba630517ecd3fec57aa8277725
intelligentracinginc <allen@intelligentracing.com>
2020-04-04 15:24:28 -0700

    Update matplotlib_sigmoid.py

--- lecture_16/matplotlib_sigmoid.py
+++ lecture_16/matplotlib_sigmoid.py
@@ -9,11 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sigmoid(x):
-    a = []
-    for item in x:
-        a.append(1/(1+math.exp(-item)))
-    return a
-
+    return 1/(1+np.exp(-x))
 
 x = np.arange(-10., 10., 0.2)
 sig = sigmoid(x)
