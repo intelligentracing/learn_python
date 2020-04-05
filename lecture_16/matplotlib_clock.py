9ea3276529d41595147e6c8425b0029f4729f019
intelligentracinginc <allen@intelligentracing.com>
2020-04-05 01:37:17 -0700

    Update matplotlib_clock.py

--- lecture_16/matplotlib_clock.py
+++ lecture_16/matplotlib_clock.py
@@ -6,11 +6,31 @@
 
 from datetime import datetime
 import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+# Initialization, define some constant
+path = os.path.dirname(os.path.abspath(__file__))
+filename = path + '/airplane.bmp'
+second_hand_lengh = 200
+second_hand_width = 2
+minute_hand_length = 150
+minute_hand_width = 4
+hour_hand_length = 100
+hour_hand_width = 10
+center = [256, 256]
 
 # First retrieve the time
 now_time = datetime.now()
-time = now_time.split(' ')
-hour, minute, second = time.split(":")
+hour = now_time.hour
+minute = now_time.minute
+second = now_time.second
 
 # draw an image background
+background = plt.imread(filename)
+fig, ax = plt.subplots()
+plt.imshow(background)
 
+plt.arrow()
+
+plt.show()
