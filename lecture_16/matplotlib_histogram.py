95000c5b31c0df0e52d8a90db7430b92f780823d
intelligentracinginc <allen@intelligentracing.com>
2020-04-05 13:27:44 -0700

    Update matplotlib_histogram.py

--- lecture_16/matplotlib_histogram.py
+++ lecture_16/matplotlib_histogram.py
@@ -9,12 +9,17 @@ import matplotlib.pyplot as plt
 
 text = 'We can know only that we know nothing. And that is the highest degree of human wisdom.' # From War and Peace
 
+hist_labels = list('abcdefghijklmnopqrstuvwxyz') # label the 26 histogram bins with their text
+
 hist = np.zeros(0)
 for c in text:
     if c.isalpha():
         index = ord(c.lower()) - ord('a')
-        np.append(hist, index)
+        hist = np.append(hist, index)
 
-plt.hist(hist, bin = [0:26])
+plt.hist(hist, 26, facecolor = 'green', alpha = 0.5)
 plt.title("ASCII histogram")
+#plt.xticks(list(range(0,26)), hist_labels)   # This option will print tick lables at the beginning of a bin
+plt.xticks(np.array(np.arange(0,26))+0.5, hist_labels)
+plt.xlim(0,26)
 plt.show()
