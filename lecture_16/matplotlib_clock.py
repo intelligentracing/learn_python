4d10509e931808883c1c13b38eea47857c9ce862
intelligentracinginc <allen@intelligentracing.com>
2020-04-05 02:44:23 -0700

    Update matplotlib_clock.py

--- lecture_16/matplotlib_clock.py
+++ lecture_16/matplotlib_clock.py
@@ -12,25 +12,41 @@ import numpy as np
 # Initialization, define some constant
 path = os.path.dirname(os.path.abspath(__file__))
 filename = path + '/airplane.bmp'
-second_hand_lengh = 200
+background = plt.imread(filename)
+
+second_hand_length = 200
 second_hand_width = 2
 minute_hand_length = 150
-minute_hand_width = 4
+minute_hand_width = 6
 hour_hand_length = 100
 hour_hand_width = 10
-center = [256, 256]
+center = np.array([256, 256])
 
-# First retrieve the time
-now_time = datetime.now()
-hour = now_time.hour
-minute = now_time.minute
-second = now_time.second
+def clock_hand_vector(angle, length):
+    return np.array([length * np.sin(angle), -length * np.cos(angle)])
 
 # draw an image background
-background = plt.imread(filename)
 fig, ax = plt.subplots()
-plt.imshow(background)
 
-plt.arrow()
+while True:
+    plt.imshow(background)
 
-plt.show()
+    # First retrieve the time
+    now_time = datetime.now()
+    hour = now_time.hour
+    if hour>12: hour = hour - 12
+    minute = now_time.minute
+    second = now_time.second
+
+    # Calculate end points of hour, minute, second
+
+    hour_vector = clock_hand_vector(hour/12*2*np.pi, hour_hand_length)
+    minute_vector = clock_hand_vector(minute/60*2*np.pi, minute_hand_length)
+    second_vector = clock_hand_vector(second/60*2*np.pi, second_hand_length)
+
+    plt.arrow(center[0], center[1], hour_vector[0], hour_vector[1], head_length = 3, linewidth = hour_hand_width, color = 'black')
+    plt.arrow(center[0], center[1], minute_vector[0], minute_vector[1], linewidth = minute_hand_width, color = 'black')
+    plt.arrow(center[0], center[1], second_vector[0], second_vector[1], linewidth = second_hand_width, color = 'red')
+
+    plt.pause(0.1)
+    plt.clf()
