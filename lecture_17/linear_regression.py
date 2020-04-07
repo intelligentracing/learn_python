05a4dc3782f3c4516bf4af72f20d92d3fe44e536
intelligentracinginc <allen@intelligentracing.com>
2020-04-07 14:50:18 -0700

    Update linear_regression.py

--- lecture_17/linear_regression.py
+++ lecture_17/linear_regression.py
@@ -7,3 +7,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Define the model
+x = np.arange(-5, 5, 0.1)
+y = 2*x -1
+l1, =plt.plot(x, y, 'gray', linewidth = 3)
+l1.set_label('Model')
+
+# Generate noisy sample
+ax = plt.axes()
+sample_count = 20
+x_sample = 10*np.random.random(sample_count)-5
+y_sample = 2*x_sample-1 + 3*np.random.rand(sample_count)
+ax.scatter(x_sample, y_sample, c = x_sample, cmap = 'hsv')
+
+A = np.zeros((sample_count, 2))
+for i in range(sample_count):
+    A[i,0] = x_sample[i]
+    A[i,1] = 1.0
+
+aa = np.linalg.lstsq(A, y_sample, rcond = None)[0]
+print(aa)
+
+y_estimate = aa[0]*x + aa[1]
+l2, = plt.plot(x, y_estimate, 'r', linewidth = 3)
+l2.set_label('Prediction')
+ax.legend()
+
+plt.show()
