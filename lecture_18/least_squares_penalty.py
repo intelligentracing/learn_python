9de80ac0ac3053d35de3cd495e434dc2d9dea878
intelligentracinginc <allen@intelligentracing.com>
2020-04-09 16:01:18 -0700

    Update least_squares_penalty.py

--- lecture_18/least_squares_penalty.py
+++ lecture_18/least_squares_penalty.py
@@ -26,4 +26,21 @@ ax1.scatter(x_sample, y_sample, c = x_sample, cmap = 'hsv')
 # subplot 2 plots the parameter space
 ax2 = fig.add_subplot(1,2,2, projection = '3d')
 
+def penalty(para_a, para_b):
+    global x_sample, y_sample
+
+    squares = (y_sample - para_a*x_sample - para_b)**2
+    return 1/len(x_sample)*np.sum(squares)
+
+a_arr, b_arr = np.meshgrid(np.arange(-5, 5, 0.1), np.arange(-5, 5, 0.1) )
+
+func_value = np.zeros(a_arr.shape)
+for x in range(len(a_arr)):
+    for y in range(len(a_arr[x])):
+            func_value[x, y] = penalty(a_arr[x, y], b_arr[x, y])
+
+ax2.plot_surface(a_arr, b_arr, func_value, color = 'red', alpha = 0.8)
+
+ax2.scatter(2, -1, penalty(2,-1),marker = '*')
+
 plt.show()
