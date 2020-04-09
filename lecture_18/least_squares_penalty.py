d5341c1d3fa3060fe1e0a626df943391af47fc0f
intelligentracinginc <allen@intelligentracing.com>
2020-04-09 11:56:42 -0700

    Create least_squares_penalty.py

---
+++ lecture_18/least_squares_penalty.py
@@ -0,0 +1,19 @@
+## This is course material for Introduction to Python Scientific Programming
+## Class 18 Example code: least_squares_penalty.py
+## Author: Allen Y. Yang,  Intelligent Racing Inc.
+##
+## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,2,1)
+ax2 = fig.add_subplot(1,2,2, projection = '3d')
+
+# subplot 1 plots the noisy samples
+
+
+# subplot 2 plots the parameter space
+
