4d707a5995cec9786fa94e1d76192c887d65b764
intelligentracinginc <allen@intelligentracing.com>
2020-04-09 20:54:19 -0700

    add labels

--- lecture_18/gradient_descent.py
+++ lecture_18/gradient_descent.py
@@ -1,5 +1,5 @@
 ## This is course material for Introduction to Python Scientific Programming
-## Class 18 Example code: least_squares_penalty.py
+## Class 18 Example code: gradient_descent.py
 ## Author: Allen Y. Yang,  Intelligent Racing Inc.
 ##
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
@@ -9,22 +9,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,2,1)
 
-# subplot 1 plots the noisy samples
-# Define the model
-x = np.arange(-5, 5, 0.1)
-y = 2*x - 1
-plt.plot(x, y, 'gray', linewidth = 3)
-
-# Generate noisy sample
 sample_count = 100
 x_sample = 10*np.random.random(sample_count)-5
 y_sample = 2*x_sample - 1 + np.random.normal(0, 1.0, sample_count)
-ax1.scatter(x_sample, y_sample, c = x_sample, cmap = 'hsv')
 
 # subplot 2 plots the parameter space
-ax2 = fig.add_subplot(1,2,2, projection = '3d')
+ax2 = fig.add_subplot(1,1,1, projection = '3d')
 
 def penalty(para_a, para_b):
     global x_sample, y_sample, sample_count
@@ -40,10 +31,10 @@ for x in range(a_arr.shape[0]):
             func_value[x, y] = penalty(a_arr[x, y], b_arr[x, y])
 
 ax2.plot_surface(a_arr, b_arr, func_value, color = 'red', alpha = 0.8)
+ax2.scatter(2, -1, penalty(2,-1),marker = '*')
 ax2.set_xlabel('a parameter')
 ax2.set_ylabel('b parameter')
 ax2.set_zlabel('f(a, b)')
 
-ax2.scatter(2, -1, penalty(2,-1),marker = '*')
 
 plt.show()
