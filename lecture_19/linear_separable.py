8c6f49c2147b66b3c5189c997d6b41b8bbe14d04
intelligentracinginc <allen@intelligentracing.com>
2020-04-14 00:31:50 -0700

    created Gaussian_model.py

--- lecture_19/linear_separable.py
+++ lecture_19/linear_separable.py
@@ -1,5 +1,5 @@
 ## This is course material for Introduction to Python Scientific Programming
-## Class 19 Example code: linear_classifier.py
+## Class 19 Example code: linear_separable.py
 ## Author: Allen Y. Yang,  Intelligent Racing Inc.
 ##
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
