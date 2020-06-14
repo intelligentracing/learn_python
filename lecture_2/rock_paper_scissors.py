6ae03358d2aa76e6995c4e841fee2ad5da706636
learnwithallen <allen@intelligentracing.com>
2020-06-14 12:59:29 -0700

    Update rock_paper_scissors.py

--- lecture_2/rock_paper_scissors.py
+++ lecture_2/rock_paper_scissors.py
@@ -4,21 +4,21 @@
 ##
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
 
-
 import random
 
-# Ask user to input 1 or 2 or 3
-print("Please input [1] Rock, [2] Paper, or [3] Scissors")
-user_input = int(input())
+# Ask user to input R or P or S
+user_input = input("Please input [R] Rock, [P] Paper, or [S] Scissors: ")   # input is a string type
 
-if user_input !=1 and user_input !=2 and user_input !=3:
+if user_input !='R' and user_input !='P' and user_input !='S': # Doing exception test
     print("Input not supported")
 else:
-    computer_input = random.randint(1,3)
+    computer_input = random.choice(['R', 'P', 'S'])
     print("Computer input is ", computer_input)
     if computer_input == user_input:
         print("Draw!")
-    elif computer_input - user_input == 1 or computer_input-user_input == -2:
+    elif (computer_input=='R' and user_input == 'S') or \
+        (computer_input=='S' and user_input == 'P') or \
+        (computer_input=='P' and user_input == 'R'):
         print("You Lose!")
     else:
         print("You Win!")
