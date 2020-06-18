df14520fadc41ab53015d530bb6dabd659009bb8
learnwithallen <allen@intelligentracing.com>
2020-06-18 09:08:24 -0700

    Update rock_paper_scissors.py

--- lecture_2/rock_paper_scissors.py
+++ lecture_2/rock_paper_scissors.py
@@ -7,18 +7,18 @@
 import random
 
 # Ask user to input R or P or S
-user_input = input("Please input [R] Rock, [P] Paper, or [S] Scissors: ")   # input is a string type
+user_input = input('Please input [R] Rock, [P] Paper, or [S] Scissors: ')   # input is a string type
 
 if user_input !='R' and user_input !='P' and user_input !='S': # Doing exception test
     print("Input not supported")
 else:
     computer_input = random.choice(['R', 'P', 'S'])
-    print("Computer input is ", computer_input)
+    print('Computer input is ', computer_input)
     if computer_input == user_input:
         print("Draw!")
     elif (computer_input=='R' and user_input == 'S') or \
         (computer_input=='S' and user_input == 'P') or \
         (computer_input=='P' and user_input == 'R'):
-        print("You Lose!")
+        print('You Lose!')
     else:
-        print("You Win!")
+        print('You Win!')
