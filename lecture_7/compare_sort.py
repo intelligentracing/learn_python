0a519e29177e6d79353d3a62b9671a31ab4c7e8b
learnwithallen <allen@intelligentracing.com>
2020-07-06 00:00:59 -0700

    Moved reverse option

--- lecture_7/compare_sort.py
+++ lecture_7/compare_sort.py
@@ -7,42 +7,32 @@
 import random
 import time
 
-def insert_sort(input_list, reverse = False):
+def insert_sort(input_list):
     ''' A custom function to sort number sequences using insert sort
     Parameters:
     Input:  input_list  - Expecting a list of numerical numbers
-            order       - Ascending or descending order, default = 0
 
     Output: status      - Boolean: True or False
             input_list  - sorted list if status is True
     '''
-    if type(reverse)!=bool:
+    if type(input_list)!=list:
         return False
 
-    for index in range(len(input_list)):
+    for index in range(1, len(input_list)):
     
         # Compare and sort elements one by one
         current = input_list[index]
 
         # Verify the type of each element
         if type(current)!=int and type(current)!=float:
-            return False
+            current = float(current)
 
         # Insert to previous sorted sub-list
-        # Insert condition based on order
-        if reverse == 0:
-            while_condition = (index>0 and input_list[index-1]>current)
-        else:
-            while_condition = (index>0 and input_list[index-1]<current)
-        while while_condition:
+        while  (index>0 and input_list[index-1]>current):
             # Insert iteratively until insert condition is False
             input_list[index] = input_list[index-1]
             input_list[index-1] = current
             index -=1
-            if reverse == 0:
-                while_condition = (index>0 and input_list[index-1]>current)
-            else:
-                while_condition = (index>0 and input_list[index-1]<current)
     
     return True
 
@@ -54,7 +44,7 @@ random_input = random.sample(range(0, sample_count),sample_count)
 print('*** Insert Sort ***')
 result = random_input.copy()
 begin_time = time.time()
-insert_sort(result, True)
+insert_sort(result)
 
 # tic-toc 
 elapsed_time = time.time() - begin_time
--- lecture_7/merge_sort.py
+++ lecture_7/merge_sort.py
@@ -7,6 +7,45 @@
 import random
 import time
 
+def insert_sort(input_list, reverse = False):
+    ''' A custom function to sort number sequences using insert sort
+    Parameters:
+    Input:  input_list  - Expecting a list of numerical numbers
+            order       - Ascending or descending order, default = 0
+
+    Output: status      - Boolean: True or False
+            input_list  - sorted list if status is True
+    '''
+    if type(reverse)!=bool:
+        return False
+
+    for index in range(len(input_list)):
+    
+        # Compare and sort elements one by one
+        current = input_list[index]
+
+        # Verify the type of each element
+        if type(current)!=int and type(current)!=float:
+            return False
+
+        # Insert to previous sorted sub-list
+        # Insert condition based on order
+        if reverse == 0:
+            while_condition = (index>0 and input_list[index-1]>current)
+        else:
+            while_condition = (index>0 and input_list[index-1]<current)
+        while while_condition:
+            # Insert iteratively until insert condition is False
+            input_list[index] = input_list[index-1]
+            input_list[index-1] = current
+            index -=1
+            if reverse == 0:
+                while_condition = (index>0 and input_list[index-1]>current)
+            else:
+                while_condition = (index>0 and input_list[index-1]<current)
+    
+    return True
+
 def merge_sort(input_list):
     ''' Merge sort function using recursion
     Parameters:
