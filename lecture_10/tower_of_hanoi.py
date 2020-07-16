287d8fc2c1e76944db9ba9d3379ee1799e0c76e3
learnwithallen <allen@intelligentracing.com>
2020-07-16 00:38:18 -0700

    Update tower_of_hanoi.py

--- lecture_10/tower_of_hanoi.py
+++ lecture_10/tower_of_hanoi.py
@@ -14,43 +14,43 @@ def the_other_rod(begin,end):
     rod_set.remove(end)
     return rod_set.pop()
 
-def tower_of_hanoi(tower_list, begin, end, disk_count):
+def tower_of_hanoi(tower, begin, end, disk_count):
     '''
     Using dynamic programming and recursion to solve tower of hanoi.
     Parameters:
-    Input:  tower_list  - A list of three sublists, each recording the dishes on each rod
+    Input:  tower  - A list of three stacks, each recording the dishes on each rod
             begin       - Current starting rod number
             end         - Current goal rod number
             disk_count  - Indicate how many disks this step intends to achieve moving
     '''
 
     # This is just an example to check the input arguments
-    if type(tower_list)!= list or len(tower_list)!=3 or \
-        type(tower_list[0])!= deque or type(tower_list[1])!= deque or type(tower_list[2])!= deque:
-        raise TypeError('Argument 1 must be a list of three integer lists')
+    if type(tower)!= list or len(tower)!=3 or \
+        type(tower[0])!= deque or type(tower[1])!= deque or type(tower[2])!= deque:
+        raise TypeError('Argument 1 must be a list of three stacks')
 
     if disk_count == 1:
         # this is a basic movement
-        if len(tower_list[begin]) <=0 or \
-            (len(tower_list[end])>0 and tower_list[begin][-1]>tower_list[end][-1]):
+        if len(tower[begin]) <=0 or \
+            (len(tower[end])>0 and tower[begin][-1]>tower[end][-1]):
             raise Exception('The Rule is broken. Illegal move exists!')
         
-        tower_list[end].append(tower_list[begin].pop())
-        print(tower_list)
+        tower[end].append(tower[begin].pop())
+        print(tower)
     else:
         middle_rod = the_other_rod(begin, end)
 
-        tower_of_hanoi(tower_list, begin, middle_rod, disk_count - 1)
-        tower_of_hanoi(tower_list, begin, end, 1)
-        tower_of_hanoi(tower_list, middle_rod, end, disk_count - 1)
+        tower_of_hanoi(tower, begin, middle_rod, disk_count - 1)
+        tower_of_hanoi(tower, begin, end, 1)
+        tower_of_hanoi(tower, middle_rod, end, disk_count - 1)
 
 rod_1 = deque([4, 3, 2, 1])
 rod_2 = deque([])
 rod_3 = deque([])
-tower_list= []
-tower_list.append(rod_1)
-tower_list.append(rod_2)
-tower_list.append(rod_3)
+tower= []
+tower.append(rod_1)
+tower.append(rod_2)
+tower.append(rod_3)
 
-print(tower_list)
-tower_of_hanoi(tower_list, 0, 1, 4)
+print(tower)
+tower_of_hanoi(tower, 0, 1, 4)
