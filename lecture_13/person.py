152b0592ba61d606ec023fc6b094047c0f08b778
Mona-Lisa <allen@intelligentracing.com>
2020-07-27 00:58:27 -0700

    revise Person class

--- lecture_13/person.py
+++ lecture_13/person.py
@@ -1,5 +1,5 @@
 ## This is course material for Introduction to Python Scientific Programming
-## Class 13 Example code: person_class.py
+## Class 13 Example code: person.py
 ## Author: Allen Y. Yang,  Intelligent Racing Inc.
 ##
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
@@ -7,18 +7,35 @@
 class Person:
     ''' An example class to show Python Class definitions'''
     
-    retirement_age = 65
+    retirement_age = 65   # Class attribute
 
     def __init__(self, first_name, last_name):
+        if type(first_name)!=str or type(last_name)!=str:
+            raise TypeError('Person class initailzed with the unsupported types.')
+
         self.first_name = first_name
         self.last_name = last_name
+        self.age = None
+
+    def set_age(self, age):
+        if age<0:
+            raise ValueError('age attribute in Person must be nonnegative.')
+
+        self.age = age
 
     def print_name(selfish):
         print('First Name: {0}, Last Name: {1}'.format(selfish.first_name, selfish.last_name))
 
     @classmethod
-    def years_until_retirement(cls, current_age):
-        until_retirement_year = cls.retirement_age - current_age
+    def set_retirement_age(careful, retirement_age):
+        if retirement_age<0:
+            raise ValueError('age attribute in Person must be nonnegative.')
+
+        careful.retirement_age = retirement_age
+
+    @classmethod    
+    def years_until_retirement(cls, age):
+        until_retirement_year = cls.retirement_age - age
         if until_retirement_year<=0:
             print('This person has retired')
         else:
--- lecture_13/test_person_class.py
+++ lecture_13/test_person_class.py
@@ -4,23 +4,15 @@
 ##
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
 
-from person_class import Person
+from person import Person
 
 x1 = Person('John', 'Smith')
 x2 = Person('Jane', 'Doe')
 x1.print_name()
 x2.print_name()
+
 print(x1.retirement_age)
 Person.years_until_retirement(42)
-Person.years_until_retirement(80)
+x1.set_age(80)
+x1.years_until_retirement(x1.age)
 Person.copyright()
-
-print(id(x1))
-x1.retirement_age = 70
-print(id(x1))
-
-x2 = x1; print(id(x2))
-
-import copy
-x3 = copy.copy(x1); print(id(x3))
-print(x3.retirement_age)
