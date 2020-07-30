1ed8780aaa0a1ffffb45576ee513320764b28a52
Mona-Lisa <allen@intelligentracing.com>
2020-07-30 10:26:24 -0700

    Update prefix_postfix.py

--- lecture_14/prefix_postfix.py
+++ lecture_14/prefix_postfix.py
@@ -5,7 +5,7 @@
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
 
 class PrefixPostfix:
-    def __init__(self):
+    def __init__(self):             # class constructor
         self._internal = 0
         self.__double_underscore = 'name mangling'
         self.__prefix_postfix_double__ = 'no change'
@@ -15,8 +15,18 @@ class PrefixPostfix:
         print('_double_underscore: ', self.__double_underscore)
         print('__prefix_postfix_double__: ', self.__prefix_postfix_double__)
 
+    def __del__(self):              # class destructor
+        print('Goodbye!')
+
+    def __eq__(self, other):
+        if type(self)!=type(other):
+            raise TypeError('PrefixPostfix == must compare same type')
+        return(self._internal == other._internal)
+
 test = PrefixPostfix()
 test.print_out()
 print(dir(test))
 print(test._internal)
 print(test._PrefixPostfix__double_underscore)
+print(test != test)
+del(test)
