38c01b050336e9f38f4da130c9a1c04e31f9c760
Mona-Lisa <allen@intelligentracing.com>
2020-08-03 00:28:43 -0700

    Update read_image.py

--- lecture_15/read_image.py
+++ lecture_15/read_image.py
@@ -1,5 +1,5 @@
 ## This is course material for Introduction to Python Scientific Programming
-## Class 14 Example code: read_image.py
+## Class 15 Example code: read_image.py
 ## Author: Allen Y. Yang,  Intelligent Racing Inc.
 ##
 ## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
