7d68da238515b916012225dc7160e14a7d84bd91
Mona-Lisa <allen@intelligentracing.com>
2020-08-09 22:02:15 -0700

    update plt now using show()

--- lecture_17/widget_slider.py
+++ lecture_17/widget_slider.py
@@ -12,9 +12,7 @@ from matplotlib.widgets import Slider, RadioButtons
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
 t = np.arange(0.0, 1.0, 0.001)
-a0 = 5
-f0 = 3
-delta_f = 5.0
+a0 = 5; f0 = 3; delta_f = 0.1; delta_a = 0.1
 s = a0 * np.sin(2 * np.pi * f0 * t)
 l, = plt.plot(t, s, lw=2)
 ax.margins(x=0)
@@ -24,7 +22,7 @@ axcolor = 'lightgoldenrodyellow'
 axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
 sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0, valstep=delta_f)
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0, valstep=delta_a)
 
 # slider update actions
 def update(val):
@@ -39,11 +37,14 @@ samp.on_changed(update)
 # Create a radio button
 rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
 radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
-
+l.set_color(radio.value_selected)
 # radio button update actions
 def colorfunc(label):
     l.set_color(label)
     fig.canvas.draw_idle()
+
 radio.on_clicked(colorfunc)
 
-plt.show()
+#plt.show()
+fig.canvas.draw_idle()
+plt.pause(np.inf)
